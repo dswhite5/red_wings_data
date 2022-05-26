2113e119e466b9b0ce294a7ba8652496d367b64a
dswhite5 <seth.h.white@gmail.com>
2022-05-26 12:10:56 -0400

    Moved stat info between files

--- combined_stats_data.R
+++ combined_stats_data.R
@@ -65,3 +65,68 @@ scoring_regular_season_DET_all <- bind_rows(scoring_regular_season_DET_2008,
 
 #write_csv(wings_stats_combined, '~/R/Shiny_apps/red_wings_data/data/DET/wings_stats_combined.csv')
 #write_csv(scoring_regular_season_DET_all, '~/R/Shiny_apps/red_wings_data/data/DET/scoring_regular_season_DET_all.csv')
+
+# team analytics 5 v 5
+
+# S% -- Shooting percentage at 5-on-5
+# SV% -- Save percentage at 5-on-5
+# PDO -- PDO
+# Shooting % + Save %
+#   CF -- Corsi For at 5 on 5
+# Shots + Blocks + Misses
+# CA -- Corsi Against at 5 on 5
+# Shots + Blocks + Misses
+# CF% -- Corsi For % at 5 on 5
+# CF / (CF + CA)
+# Above 50% means the team was controlling the
+# puck more often than not with this player on the
+# ice in this situation.
+# xGF -- 'Expected Goals For' given where shots came from, for and against, while this player was on the ice at even strength.
+# It's based on where the shots are coming from, compared to the league-wide shooting percentage for that shot location.
+# xGA -- 'Expected Goals Against' given where shots came from, for and against, while this player was on the ice at even strength.
+# It's based on where the shots are coming from, compared to the league-wide shooting percentage for that shot location.
+# aGF -- Actual goals for (5-on-5)
+#   aGA -- Actual goals against (5-on-5)
+# axDiff -- Actual goal differential minus expected goal differential. A positive differential would indicate a team is converting or stopping an inordinate amount of good chances compared to league average. A negative differential would indicate a team is getting more good chances, but not converting or is allowing more than league norms.
+# SCF -- Scoring chances for. Scoring chances are all shot attempts from within a certain range from the net.
+# SCA -- Scoring chances against
+# SCF% -- Percentage of scoring chances in this team's favor
+# HDF -- High-danger scoring chances for. High-danger chances include shot attempts from the 'slot' area and rebounds, approximately.
+# HDA -- High-danger scoring chances against
+# HDF% -- Percentage of high-danger scoring chances in this team's favor
+# HDGF -- High-danger scoring chances for that lead to goals
+# HDC% -- Percentage of high-danger scoring chances that are converted to goals, for this team
+# HDGA -- High-danger scoring chances against that lead to goals
+# HDCO% -- Percentage of high-danger scoring chances that are converted to goals, for this team's opponents
+
+
+#team Statistics
+
+# AvAge -- Average age of team weighted by time on ice.
+# GP -- Games Played
+# W -- Wins
+# L -- Losses
+# OL -- Overtime/Shootout Losses (2000 season onward)
+# PTS -- Points
+# PTS% -- Points percentage (i.e., points divided by maximum points)
+# GF -- Goals For
+# GA -- Goals Against
+# SRS -- Simple Rating System; a team rating that takes into account average goal differential and strength of schedule. The rating is denominated in goals above/below average, where zero is average.
+# SOS -- Strength of Schedule; a rating of strength of schedule. The rating is denominated in goals above/below average, where zero is average.
+# GF/G -- Goals For Per Game
+# GA/G -- Goals Against Per Game
+# PP -- Power Play Goals
+# PPO -- Power Play Opportunities
+# PP% -- Power Play Percentage
+# PPA -- Power Play Goals Against
+# PPOA -- Power Play Opportunities Against
+# PK% -- Penalty Killing Percentage
+# SH -- Short-Handed Goals
+# SHA -- Short-Handed Goals Against
+# S -- Shots on Goal
+# S% -- Shooting Percentage
+# SA -- Shots Against
+# SV% -- Save Percentage
+# PDO -- PDO
+# Shooting % + Save %
+#   SO -- Shutouts
