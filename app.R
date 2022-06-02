40b040d2b03821ab9de3344b05b7736953b92206
dswhite5 <seth.h.white@gmail.com>
2022-06-02 10:25:01 -0400

    Paragraphs to EDA, moved graph from SA to EDA

--- app.R
+++ app.R
@@ -108,22 +108,58 @@ ui <- navbarPage('Red wings Data Analysis',
                                                           selected = '20 Goals Scorers'
                                                           ),
                                               plotOutput('graphs'),
-                                              p('Once you have done a little exploring its time to start heading a direction. The direction this site takes is towards trying to find out what stat
-                                                drives winning. To do that I created what is called a scatterplot matrix. The one below is for6 of the variables in the Red Wings Stats data and 
-                                                compared every  variable to W, the number of wins, there are other matrices not shown but if the matrix is too large the site moves to slow. What 
+                                              p('Once you have done a little exploring its time to start heading a direction. The direction this site takes is towards trying to find out what statistic
+                                                drives winning. To do that I created what is called a scatterplot matrix. The one below is for 6 of the variables in the Red Wings Stats data and 
+                                                compares thosecvariables to W, the number of wins. There are other matrices not shown but if the matrix is too large the site moves to slow. What 
                                                 the matrix shows is a scatter plot for each variable with W and gives the correlation coefficient(C) for each pairing. C is from -1 to 1 with the
-                                                sign giving whether is positively or negatively correlated and the value giving whether the variables are linearly correlated. the closer C is to 1
-                                                the more correlated the variables. So a C of -.9 would have a strong negative linear correlation. This can be seen in the corresponding scatter 
-                                                plots. W and CF% have a correlation coefficient of 0.854, indiciating a strong positive linear relationship, and the scatterplot of those two variables
-                                                the points together look like a line with an upward slope. W and S% 5v5 have a low value of C of 0.212, that scatter plot does not have the obvious
-                                                trend of W and CF%'),
+                                                sign giving whether is positively or negatively correlated and the value giving whether the variables are linearly correlated. The closer C is to 1
+                                                the more correlated the variables. So a C of -0.9 would have a strong negative linear correlation. This can be seen in the corresponding scatter 
+                                                plots. W and CF% have a correlation coefficient of 0.854, indiciating a strong positive linear relationship, and in the scatterplot of those two 
+                                                variables the points look like a line with an upward slope. Looking at W and S% 5v5, those variables have a low C value of 0.212, that scatter plot
+                                                does not have the obvious trend of W and CF%'),
                                               plotOutput('ggpairs_corsi'),
                                               #p('Lets talk about this second plot'),
                                               #plotOutput('ggpairs_shots'),
-                                              p('Why comparing linearity?'),
-                                              plotOutput('GF_v_W'),
-                                              p('lets talk about this 4th plot'),
-                                              plotOutput('CF_percent_v_W')
+                                              p('Lets talk a for brief moment as to why I am choosing linear regression as a stastical model and what that is. There are other models out there that might be a little
+                                                newer, trendier, or more complex than a linear model, so why choose the old stand by. Well, partially because it is the old stand by, most people have
+                                                taken an algebra class and had to plot two points and a line (even if they never enjoyed doing so or thought it would never be useful in real life)
+                                                and at least recognize the equation y = mx +b. Which is what a linear model is. The dependent variable is y (its value depends on x), and x is the
+                                                independent variable (its value is independent of any other variable). In a linear model a change in x leads to a proportional change in y, scaled by 
+                                                the slope of the line m. the intercept b is the value of y when x = 0, which in real linear models doesnt always apply. So this leads to the second reason
+                                                to choose a linear model, it is interperatable. The Red Wings are bad, I want to see what they can do to be good, which means I have to be able to easily
+                                                apply the analysis and linear models are easily interperatble. For the model y= mx + b a change of 1 in x leads to an m size change in y. So a linear model
+                                                seems like it will make easy to see which of the statistics in my data sets are needed to to make the number of wins go up. See the linear regession plot 
+                                                of W vs CF% below for an example of what this looks like. This model would be interpreted as a 1% change in Corsi For Percentage leads to a change of 2.41 
+                                                wins per season. It is interesting to note that the intercept in this case is not interperatable. The intercept is the number when the dependent variable 
+                                                is equal to 0, not a realistic scenario, and that it would result in a -87 wins, an impossible scenario.'),
+                                              plotOutput('reg_CFper_W'),
+                                              p('So, I want to build a linear model because it is familiar and it is easy to interpret and I want to try and find what the wings can improve on to increase
+                                                the number of wins. So I want a linear model with the dependent variable(the y variable in the previous equation) to be wins. So what is the x variable? Can
+                                                there be more than one "x" variable? If I can do more than one "x" variable is it better to have several models with one independent variable or to have one
+                                                model with several independent variables? The answer to the more than one independent variable (the "x" is the previous equation is yes, there can be more than
+                                                one. The question of lots of single variable models vs one models with multiple variables is "it depends". The attributes(columns in the data sets) available
+                                                to us seem to be related to each other. What I mean by that is that a change in one variable tends to lead to changes in other variables, if the variable S% 
+                                                (shooting percentage) goes up, it is pretty likely that the number of goals is also going to go up. When the is interalted like this doing a lot of single
+                                                variable models can be dangerous. There might be a model that looks great but the independent variable might just be correlated to a different variable that is
+                                                driving the change. Lets say you modeled wins based on player number, you might find that wins go up if there is a number 13 on the ice, and so you say we need 
+                                                another player with the number 13 and we need to give him lots of ice time. Well Pavel Datsyuk(he was very good) was number 13, and he is the variable driving the
+                                                increase in the number of wins, not the number 13. So, to avoid this there are ways to select the variables that actually are related to the changes in the
+                                                dependent variable, in this case the number of wins. For this site I am going to use a mixture of the correlation matrix above and a method called backward
+                                                selection, where you start with some variables and eliminate the ones that arent contributing until you have a model based on independent variables(the "x"s) 
+                                                that actually effect the dependent variable (the "y").'),
+                                                p('Lets start with the correlation matrix. I should clarify that for this model I am only going to use the variables from the Red wings Statistics data as I 
+                                                  can easily correlate with the number of wins. I am going to reveiw the C values for each variable combination with W, again not all of them are shown, and 
+                                                  start with the variables that have a C value above 0.8. I could do backwards selection on all 36 variable in the data set, but that seems like tedious work
+                                                  that I can skip based on how correlated the variables are with W. The variable I am picking are GF - goals for,  CF% - Corsi For Percentage, PPO-Power Play
+                                                  Opportunities, S- shots, PP - Power play Goals, GF/G - Average Goals per Game, PPOA - Power Play Opportunities against. Once I have all of my variables I 
+                                                  can start the backwards selections process to develop my linear model, which is done on the statistical analysis page.'),
+                                              br(),
+                                              br(),
+                                              br()
+                                              #I might take these plots out of here
+                                              #plotOutput('GF_v_W'),
+                                              #p('lets talk about this 4th plot'),
+                                              #plotOutput('CF_percent_v_W')
                                               )
                                     )
                           ),
@@ -131,7 +167,6 @@ ui <- navbarPage('Red wings Data Analysis',
                           fluidPage(titlePanel(h1('What Does the Data Say?')),
                                     mainPanel(h1('Linear Regression'),
                                               p('Lets Talk about linear regression!'),
-                                              plotOutput('reg_CFper_W'),
                                               p('add model summary to output as well'),
                                               verbatimTextOutput('all_var'),
                                               tableOutput('all_var_coeff'),
@@ -218,9 +253,9 @@ server <- function(input, output){
   graph = reactive(input$eda_graphs)
   output$graphs <- renderPlot({
     if(graph() == '20 Goals Scorers'){
-      goals_scorers
+      goals_scorers #code in static_content.R
     }else if(graph() == 'Max Goals by Player'){
-      max_goals
+      max_goals #code in static_content.r
   }
   })
   
@@ -245,12 +280,14 @@ server <- function(input, output){
   output$GF_v_W <- renderPlot({
     GF_v_W #plot code is in static_content.R
   })
-  ########For stats analysis tab##########################################
                              
   #Create scatter plot with linear regression line and formula on it
   output$reg_CFper_W <- renderPlot({
     reg_CFper_W # plot code is in static_content.R
-})
+    })
+  
+  ########For stats analysis tab##########################################
+  
   #formula and statistics for multiple linear regression with all chosen variables
   output$all_var <- renderText({
     all_var_stat #code in static_content.R
