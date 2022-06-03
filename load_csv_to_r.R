2a7b68cb5f83956c157f89c7cad070587e3ebeae
dswhite5 <seth.h.white@gmail.com>
2022-06-03 10:59:59 -0400

    Back to old file locations

--- load_csv_to_r.R
+++ load_csv_to_r.R
@@ -4,9 +4,9 @@
 library(tidyverse)
 
 
-data_descrp <- read_csv('~/data/scoring_stat_data_description.csv') #table with description for each variable in the stats and scoring data sets
-wings_stats_combined<-read_csv('~/data/DET/wings_stats_combined.csv') # table wtih statistics for the Team detroit redwings for seasons 2008 to 2021
-scoring_regular_season_DET_all <- read_csv('~/data/DET/scoring_regular_season_DET_all.csv') # table with stats for players on redwings from 2008 to 2021
+data_descrp <- read_csv('~/R/Shiny_apps/red_wings_data/data/scoring_stat_data_description.csv') #table with description for each variable in the stats and scoring data sets
+wings_stats_combined<-read_csv('~~/R/Shiny_apps/red_wings_data/data/DET/wings_stats_combined.csv') # table wtih statistics for the Team detroit redwings for seasons 2008 to 2021
+scoring_regular_season_DET_all <- read_csv('~~/R/Shiny_apps/red_wings_data/data/DET/scoring_regular_season_DET_all.csv') # table with stats for players on redwings from 2008 to 2021
 
 
 
